afa892ba93eef6d4503b1096190bee656b1c71dd
ScottAI <30582612+ScottAI@users.noreply.github.com>
2019-01-19 23:37:08 +0800

    Create 第六章.bug-manager.__manifest__.py

---
+++ 第六章.bug-manager.__manifest__.py
@@ -0,0 +1,8 @@
+'name': "bug管理",  
+'summary': """ 
+   用于软件开发过程当中bug的管理""",  
+	'description': """ 
+	    用于软件开发过程当中bug的管理 
+	""",  
+	'author': "Scott",  
+	'website': "http://www.scott-odoo.com",  
